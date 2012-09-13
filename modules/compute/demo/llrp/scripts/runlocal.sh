90aa0212f428792426c613e7719de6170b43ecf7
mdijkstra <mdijkstra@work>
2012-09-13 14:10:02 +0200

    We don't want generated script in git.

--- modules/compute/demo/llrp/scripts/runlocal.sh
+++
@@ -1,7 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-touch $DIR/llrp_workflow_csv.started
-export PBS_O_WORKDIR=${DIR}
-echo Starting with s00_1_PlinkPhenotypeAnalysis_1...
-sh s00_1_PlinkPhenotypeAnalysis_1.sh
-#Dependencies: 
-
--- modules/compute/demo/llrp/scripts/submit.sh
+++
@@ -1,6 +0,0 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-touch $DIR/llrp_workflow_csv.started
-#s00_1_PlinkPhenotypeAnalysis_1
-s00_1_PlinkPhenotypeAnalysis_1=$(qsub -N s00_1_PlinkPhenotypeAnalysis_1  s00_1_PlinkPhenotypeAnalysis_1.sh)
-echo $s00_1_PlinkPhenotypeAnalysis_1
-sleep 8
