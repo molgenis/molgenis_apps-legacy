573b1e3a4b4ee95526a4c2f2369168e2f735ff9d
mdijkstra <mdijkstra@work>
2012-09-14 15:29:32 +0200

    Removed execution bit from the code that generates jobs in both NGSDemo.java and PilotDemo.java. Actually, we should rename the classes, I think...

--- modules/compute4/org/molgenis/compute/test/NGSDemo.java
+++ modules/compute4/org/molgenis/compute/test/NGSDemo.java
@@ -11,17 +11,17 @@ import org.molgenis.compute.commandline.WorksheetHelper;
 import org.molgenis.compute.commandline.options.Options;
 import org.molgenis.compute.design.ComputeParameter;
 import org.molgenis.compute.design.Workflow;
-import org.molgenis.compute.test.executor.ComputeExecutor;
-import org.molgenis.compute.test.executor.ComputeExecutorPilotDB;
 import org.molgenis.compute.test.generator.ComputeGenerator;
 import org.molgenis.compute.test.generator.ComputeGeneratorDBWorksheet;
 import org.molgenis.compute.test.reader.WorkflowReader;
 import org.molgenis.compute.test.reader.WorkflowReaderDBJPA;
 import org.molgenis.util.Tuple;
 
-public class NGSDemo {
+public class NGSDemo
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println(">> Start..");
 		// Loading workflow with JPA
 		WorkflowReader reader = new WorkflowReaderDBJPA();
@@ -34,17 +34,20 @@ public class NGSDemo {
 
 		// get worksheet from commmand line parameters
 		String worksheetFile = null;
-		for (ComputeParameter cp : workflow.getWorkflowComputeParameterCollection()) {
-			if (cp.getName().equalsIgnoreCase("McWorksheet"))
-				worksheetFile = cp.getDefaultValue();
+		for (ComputeParameter cp : workflow.getWorkflowComputeParameterCollection())
+		{
+			if (cp.getName().equalsIgnoreCase("McWorksheet")) worksheetFile = cp.getDefaultValue();
 		}
 
 		// load targets from a worksheet
 		List<Tuple> worksheet = null;
-		try {
+		try
+		{
 			worksheet = (new WorksheetHelper()).readTuplesFromFile(new File(worksheetFile));
 			// "/Users/mdijkstra/Documents/work/git/molgenis_apps/modules/compute/workflows/in-house_worksheet.csv"));
-		} catch (Exception e1) {
+		}
+		catch (Exception e1)
+		{
 			e1.printStackTrace();
 		}
 
@@ -53,37 +56,29 @@ public class NGSDemo {
 		generator.generateWithTuple(workflow, worksheet, null);
 
 		System.out.println("... generated");
-
-		System.out.println("execute with pilots on the grid");
-		// execute generated tasks with pilots
-		ComputeExecutor executor = new ComputeExecutorPilotDB();
-		executor.startHost("lsgrid");
-
-		while (true) {
-			executor.executeTasks();
-			try {
-				Thread.sleep(20000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
 
-	private static void addCommandLineParameters(String[] args, Workflow workflow) {
+	private static void addCommandLineParameters(String[] args, Workflow workflow)
+	{
 		Options opt = new Options(args, Options.Prefix.DASH, Options.Multiplicity.ONCE, 0);
 
-		List<String> params = new ArrayList<String>(Arrays.asList("parameters", "workflow", "worksheet", "protocols", "templates", "scripts", "id", "dir", "backend"));
+		List<String> params = new ArrayList<String>(Arrays.asList("parameters", "workflow", "worksheet", "protocols",
+				"templates", "scripts", "id", "dir", "backend"));
 		Iterator<String> it = params.iterator();
-		while (it.hasNext()) {
+		while (it.hasNext())
+		{
 			opt.getSet().addOption(it.next(), false, Options.Separator.EQUALS);
 		}
 
 		boolean isCorrect = opt.check(opt.getSet().getSetName(), false, false);
-		if (!isCorrect) {
+		if (!isCorrect)
+		{
 			System.out.println(opt.getCheckErrors());
 
-			System.out.println("command line format:\n" + "-worksheet=<InputWorksheet.csv>\n" + "-parameters=<InputParameters.csv>\n" + "-workflow=<InputWorkflow.csv>\n"
-					+ "-protocols=<InputProtocolsDir>\n" + "-templates=<InputTemplatesDir>\n" + "-scripts=<OutputScriptsDir>\n" + "-id=<ScriptGenerationID>\n" + "-dir=<McDir>\n"
+			System.out.println("command line format:\n" + "-worksheet=<InputWorksheet.csv>\n"
+					+ "-parameters=<InputParameters.csv>\n" + "-workflow=<InputWorkflow.csv>\n"
+					+ "-protocols=<InputProtocolsDir>\n" + "-templates=<InputTemplatesDir>\n"
+					+ "-scripts=<OutputScriptsDir>\n" + "-id=<ScriptGenerationID>\n" + "-dir=<McDir>\n"
 					+ "-backend=<cluster|grid>");
 			System.exit(1);
 		}
@@ -91,13 +86,17 @@ public class NGSDemo {
 		// now add each command line parameter as ComputeParameter to workflow
 
 		it = params.iterator();
-		while (it.hasNext()) {
+		while (it.hasNext())
+		{
 			String name = it.next();
-			workflow.getWorkflowComputeParameterCollection().add(createComputeParameter("Mc" + StringUtils.firstLetterCaps(name), opt.getSet().getOption(name).getResultValue(0)));
+			workflow.getWorkflowComputeParameterCollection().add(
+					createComputeParameter("Mc" + StringUtils.firstLetterCaps(name), opt.getSet().getOption(name)
+							.getResultValue(0)));
 		}
 	}
 
-	private static ComputeParameter createComputeParameter(String name, String value) {
+	private static ComputeParameter createComputeParameter(String name, String value)
+	{
 		ComputeParameter cp = new ComputeParameter();
 		cp.setName(name);
 		cp.setDefaultValue(value);
