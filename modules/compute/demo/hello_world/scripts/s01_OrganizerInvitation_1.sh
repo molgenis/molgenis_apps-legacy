6548fcffaf0b1fb70865ef709b21cb5aa0a528ea
mdijkstra <mdijkstra@work>
2012-10-09 14:10:22 +0200

    New protocols compute demo

---
+++ modules/compute/demo/hello_world/scripts/s01_OrganizerInvitation_1.sh
@@ -0,0 +1,11 @@
+# Empty header
+#FOREACH organizer
+
+echo "Dear Oscar,"
+echo "Please organize activities for the child group."
+echo "List of guests:"
+	echo "Charly"
+	echo "Cindy"
+
+
+# Empty footer
