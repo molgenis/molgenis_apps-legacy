3c530d13eef889f67f0e0c69c6e17caae4e7e947
rw42 <robert.wagner42@gmail.com>
2012-10-23 09:44:26 +0200

    Removed application specific code from mutationPager and patientPager.

--- modules/variant/org/molgenis/mutation/ui/header/Header.java
+++ modules/variant/org/molgenis/mutation/ui/header/Header.java
@@ -50,8 +50,8 @@ public class Header extends EasyPluginController<HeaderModel> {
 				.format(cssFormat, "res/displaytag/css/displaytag.css");
 		// headers += String.format(cssFormat, "res/displaytag/css/screen.css");
 		// headers += String.format(cssFormat, "res/displaytag/css/site.css");
-		headers += String.format(cssFormat, "res/css/col7a1/colors.css");
-		headers += String.format(cssFormat, "res/css/col7a1/data.css");
+//		headers += String.format(cssFormat, "res/css/col7a1/colors.css");
+//		headers += String.format(cssFormat, "res/css/col7a1/data.css");
 		// headers += String.format(cssFormat, "res/css/main.css");
 		// headers += String.format(cssFormat, "res/css/menu.css");
 		// headers += String.format(jsFormat, "res/scripts/all.js");
