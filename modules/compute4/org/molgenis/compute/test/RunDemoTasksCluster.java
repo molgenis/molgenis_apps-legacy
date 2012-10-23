04800587a298fe8bc944daba0bb9ca83c97fcf82
RoanKanninga <roankanninga@gmail.com>
2012-10-23 14:38:17 +0200

    changed format of compute

--- modules/compute4/org/molgenis/compute/test/RunDemoTasksCluster.java
+++ modules/compute4/org/molgenis/compute/test/RunDemoTasksCluster.java
@@ -4,33 +4,30 @@ import org.molgenis.compute.test.executor.ComputeExecutor;
 import org.molgenis.compute.test.executor.ComputeExecutorPilotDB;
 
 /**
- * Created with IntelliJ IDEA.
- * User: georgebyelas
- * Date: 13/09/2012
- * Time: 16:10
+ * Created with IntelliJ IDEA. User: georgebyelas Date: 13/09/2012 Time: 16:10
  * To change this template use File | Settings | File Templates.
  */
 public class RunDemoTasksCluster
 {
-    public static void main(String[] args)
-    {
-        System.out.println("execute with pilots on the cluster");
-        // execute generated tasks with pilots
-        ComputeExecutor executor = new ComputeExecutorPilotDB();
-        executor.startHost("gcc");
-        executor.executeTasks(ComputeExecutorPilotDB.BACK_END_CLUSTER);
+	public static void main(String[] args)
+	{
+		System.out.println("execute with pilots on the cluster");
+		// execute generated tasks with pilots
+		ComputeExecutor executor = new ComputeExecutorPilotDB();
+		executor.startHost("gcc");
+		executor.executeTasks(ComputeExecutorPilotDB.BACK_END_CLUSTER);
 
-        while (true)
-        {
-            executor.executeTasks(ComputeExecutorPilotDB.BACK_END_CLUSTER);
-            try
-            {
-                Thread.sleep(10000);
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
+		while (true)
+		{
+			executor.executeTasks(ComputeExecutorPilotDB.BACK_END_CLUSTER);
+			try
+			{
+				Thread.sleep(10000);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- modules/compute4/org/molgenis/compute/test/generator/FoldingMaster.java
+++ modules/compute4/org/molgenis/compute/test/generator/FoldingMaster.java
@@ -9,17 +9,16 @@ import java.util.Hashtable;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: georgebyelas
- * Date: 22/08/2012
- * Time: 12:46
+ * Created with IntelliJ IDEA. User: georgebyelas Date: 22/08/2012 Time: 12:46
  * To change this template use File | Settings | File Templates.
  */
 public interface FoldingMaster
 {
 
-    Hashtable<String, String> createValues(Collection<ComputeParameter> listParameters, List<Target> targets, Hashtable<String, String> userValues);
-    List<Tuple> createTuples(Collection<ComputeParameter> listParameters, List<Tuple> targets, Hashtable<String, String> userValues);
+	Hashtable<String, String> createValues(Collection<ComputeParameter> listParameters, List<Target> targets,
+			Hashtable<String, String> userValues);
 
+	List<Tuple> createTuples(Collection<ComputeParameter> listParameters, List<Tuple> targets,
+			Hashtable<String, String> userValues);
 
 }
