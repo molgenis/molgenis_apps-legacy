f510b6b7742679008f2300e43a0a5c05b2bc19ee
dennishendriksen <d.hendriksen@umcg.nl>
2012-11-02 08:33:54 +0100

    first commit HL7 LifeLines parser (OC)

---
+++ modules/omicsconnect-hl7parser/org/omicsconnect/io/hl7/HL7Main.java
@@ -0,0 +1,33 @@
+package org.omicsconnect.io.hl7;
+
+import org.molgenis.framework.db.Database;
+
+import app.DatabaseFactory;
+
+/**
+ * 
+ * @author roankanninga
+ */
+public class HL7Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		PropertiesPath path = new PropertiesPath();
+		String file1 = "/Users/pc_iverson/Desktop/Input/HL7Files/Catalog-EX04.xml";
+		// String file1 = "/Users/Roan/Work/HL7/Catalog-EX04.xml";
+		String file2 = "/Users/pc_iverson/Desktop/Input/HL7Files/Catalog-EX04-valuesets.xml";
+		// String file2 = path.getPath()+"Catalog-EX04-valuesets.xml";
+		// String file2 = "/Users/Roan/Work/HL7/Catalog-EX04-valuesets.xml";
+
+		// Read file, fill arraylists
+
+		HL7Data ll = new HL7LLData(file1, file2);
+
+		Database db = DatabaseFactory.create();
+
+		HL7OmicsConnectImporter importer = new HL7OmicsConnectImporter();
+
+		importer.start(ll, db);
+	}
+
+}
---
+++ modules/omicsconnect-hl7parser/org/omicsconnect/io/hl7/PropertiesPath.java
@@ -0,0 +1,12 @@
+package org.omicsconnect.io.hl7;
+
+public class PropertiesPath
+{
+	private String path = "/Users/pc_iverson/Desktop/Input";
+
+	public String getPath()
+	{
+		return path;
+	}
+
+}
