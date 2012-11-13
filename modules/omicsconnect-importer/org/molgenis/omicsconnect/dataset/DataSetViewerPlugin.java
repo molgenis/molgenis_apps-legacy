809338680d52a1d4a1d9457b1ef8871c1e69de46
erwinwinder <j.a.winder@umcg.nl>
2012-11-13 09:55:09 +0100

    Fixed catalogue/DataSetTable link Added unittest

--- modules/omicsconnect-importer/org/molgenis/omicsconnect/dataset/DataSetViewerPlugin.java
+++ modules/omicsconnect-importer/org/molgenis/omicsconnect/dataset/DataSetViewerPlugin.java
@@ -1,23 +1,36 @@
 package org.molgenis.omicsconnect.dataset;
 
 import java.io.OutputStream;
+import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.server.MolgenisRequest;
 import org.molgenis.framework.tupletable.TableException;
+import org.molgenis.framework.tupletable.TupleTable;
 import org.molgenis.framework.tupletable.view.JQGridView;
+import org.molgenis.framework.tupletable.view.JQGridViewCallback;
 import org.molgenis.framework.ui.EasyPluginController;
 import org.molgenis.framework.ui.FormController;
 import org.molgenis.framework.ui.ScreenController;
 import org.molgenis.framework.ui.ScreenView;
 import org.molgenis.framework.ui.html.MolgenisForm;
+import org.molgenis.model.elements.Field;
 import org.molgenis.observ.DataSet;
+import org.molgenis.observ.ObservableFeature;
 import org.molgenis.util.HandleRequestDelegationException;
 import org.molgenis.util.Tuple;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
 /** Simple plugin that only shows a data table for testing */
-public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugin>
+public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugin> implements JQGridViewCallback
 {
 	private static final long serialVersionUID = 1L;
+	private static Logger logger = Logger.getLogger(DataSetViewerPlugin.class);
 
 	JQGridView tableView;
 
@@ -30,6 +43,7 @@ public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugi
 	public void reload(Database db)
 	{
 		// get from parent
+		@SuppressWarnings("unchecked")
 		FormController<DataSet> parent = (FormController<DataSet>) this.getParent();
 
 		// get current dataset in a table
@@ -42,8 +56,7 @@ public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugi
 		}
 		catch (TableException e)
 		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error("TableException in reload of DataSetViewPlugin", e);
 		}
 
 	}
@@ -65,5 +78,53 @@ public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugi
 		view.add(tableView);
 
 		return view;
+	}
+
+	@Override
+	public void beforeLoadConfig(MolgenisRequest request, TupleTable tupleTable)
+	{
+		HttpSession session = request.getRequest().getSession();
+
+		// Check if there are selected observable features (from the catalogue
+		// viewer)
+		@SuppressWarnings("unchecked")
+		List<ObservableFeature> selectedObservableFeatures = (List<ObservableFeature>) session
+				.getAttribute("selectedObservableFeatures");
+
+		if (selectedObservableFeatures != null)
+		{
+			try
+			{
+				for (final Field field : tupleTable.getAllColumns())
+				{
+					ObservableFeature observableFeature = Iterables.find(selectedObservableFeatures,
+							new Predicate<ObservableFeature>()
+							{
+								@Override
+								public boolean apply(ObservableFeature of)
+								{
+									return of.getIdentifier().equals(field.getName());
+								}
+
+							}, null);
+
+					if (observableFeature == null)
+					{
+						tupleTable.hideColumn(field.getName());
+					}
+					else
+					{
+						tupleTable.showColumn(field.getName());
+					}
+				}
+
+				session.removeAttribute("selectedObservableFeatures");
+			}
+			catch (TableException e)
+			{
+				logger.error("TableException setting selected ObservableFeatures", e);
+			}
+
+		}
 	}
 }
---
+++ modules/omicsconnect-importer/org/molgenis/omicsconnect/test/DataSetViewerPluginTest.java
@@ -0,0 +1,63 @@
+package org.molgenis.omicsconnect.test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.Mockito;
+import org.molgenis.framework.server.MolgenisRequest;
+import org.molgenis.framework.tupletable.TupleTable;
+import org.molgenis.model.elements.Field;
+import org.molgenis.observ.ObservableFeature;
+import org.molgenis.omicsconnect.dataset.DataSetViewerPlugin;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.testng.annotations.Test;
+
+public class DataSetViewerPluginTest
+{
+	/**
+	 * Check if we have selected ObservableFeatures in the HttpSession the
+	 * columns of the tupletable are shown or hidden correctly
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSelectedObservableFeatures() throws Exception
+	{
+		DataSetViewerPlugin plugin = new DataSetViewerPlugin("DataSet", null);
+
+		TupleTable table = mock(TupleTable.class);
+
+		// Add the columns of the tupletable
+		List<Field> columns = new ArrayList<Field>();
+		columns.add(new Field("1"));
+		columns.add(new Field("2"));
+		columns.add(new Field("3"));
+		when(table.getAllColumns()).thenReturn(columns);
+
+		MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
+		MolgenisRequest request = new MolgenisRequest(httpServletRequest);
+
+		// Add the selected observablefeatures to the HttpSession
+		List<ObservableFeature> selectedObservableFeatures = new ArrayList<ObservableFeature>();
+		ObservableFeature of1 = new ObservableFeature();
+		of1.setIdentifier("1");
+		selectedObservableFeatures.add(of1);
+		ObservableFeature of2 = new ObservableFeature();
+		of2.setIdentifier("2");
+		selectedObservableFeatures.add(of2);
+
+		httpServletRequest.getSession().setAttribute("selectedObservableFeatures", selectedObservableFeatures);
+
+		// Call the plugin, this would normally be called by the JQGridView
+		plugin.beforeLoadConfig(request, table);
+
+		// Check if the hide and show column methods of the tupletable were
+		// called
+		Mockito.verify(table).showColumn("1");
+		Mockito.verify(table).showColumn("2");
+		Mockito.verify(table).hideColumn("3");
+	}
+}
