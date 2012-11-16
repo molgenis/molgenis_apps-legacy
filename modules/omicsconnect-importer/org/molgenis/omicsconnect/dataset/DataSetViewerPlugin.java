bd9050960f037b4843aafc2f855131fefb22950a
erwinwinder <j.a.winder@umcg.nl>
2012-11-16 09:25:01 +0100

    Fixed broken link between ProtocolViewer and DataSetViewer Changed columnnames of the DataSetViewer from identifier to name

--- modules/omicsconnect-importer/org/molgenis/omicsconnect/dataset/DataSetViewerPlugin.java
+++ modules/omicsconnect-importer/org/molgenis/omicsconnect/dataset/DataSetViewerPlugin.java
@@ -7,19 +7,18 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.DatabaseException;
 import org.molgenis.framework.server.MolgenisRequest;
 import org.molgenis.framework.tupletable.TableException;
-import org.molgenis.framework.tupletable.TupleTable;
 import org.molgenis.framework.tupletable.view.JQGridView;
-import org.molgenis.framework.tupletable.view.JQGridViewCallback;
 import org.molgenis.framework.ui.EasyPluginController;
-import org.molgenis.framework.ui.FormController;
 import org.molgenis.framework.ui.ScreenController;
 import org.molgenis.framework.ui.ScreenView;
 import org.molgenis.framework.ui.html.MolgenisForm;
 import org.molgenis.model.elements.Field;
 import org.molgenis.observ.DataSet;
 import org.molgenis.observ.ObservableFeature;
+import org.molgenis.omicsconnect.view.DataSetChooser;
 import org.molgenis.util.HandleRequestDelegationException;
 import org.molgenis.util.Tuple;
 
@@ -27,104 +26,140 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 /** Simple plugin that only shows a data table for testing */
-public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugin> implements JQGridViewCallback
+public class DataSetViewerPlugin extends EasyPluginController<DataSetViewerPlugin>
 {
 	private static final long serialVersionUID = 1L;
 	private static Logger logger = Logger.getLogger(DataSetViewerPlugin.class);
-
-	JQGridView tableView;
+	private JQGridView tableView;
+	private DataSetChooser dataSetChooser;
 
 	public DataSetViewerPlugin(String name, ScreenController<?> parent)
 	{
 		super(name, parent);
+		setModel(this);
 	}
 
 	@Override
 	public void reload(Database db)
 	{
-		// get from parent
-		@SuppressWarnings("unchecked")
-		FormController<DataSet> parent = (FormController<DataSet>) this.getParent();
-
-		// get current dataset in a table
-		DataSetTable table;
-		try
+		if (tableView == null)
 		{
-			table = new DataSetTable(parent.getModel().getCurrent(), db);
-
-			tableView = new JQGridView("test", this, table);
+			createViews(db, null, null);
 		}
-		catch (TableException e)
-		{
-			logger.error("TableException in reload of DataSetViewPlugin", e);
-		}
-
 	}
 
 	// handling of the ajax; should be auto-wired via the JQGridTableView
 	// contructor (TODO)
-	public void download_json_test(Database db, Tuple request, OutputStream out)
+	public void download_json_dataset(Database db, Tuple request, OutputStream out)
 			throws HandleRequestDelegationException
 	{
-		// handle requests for the table named 'test'
+		// handle requests for the table named 'dataset'
 		tableView.handleRequest(db, request, out);
+	}
+
+	public void selectDataSet(Database db, Tuple request) throws HandleRequestDelegationException
+	{
+		Integer selectedDataSetId = request.getInt("dataSetId");
+		HttpSession session = ((MolgenisRequest) request).getRequest().getSession();
+
+		if (selectedDataSetId != null)
+		{
+			@SuppressWarnings("unchecked")
+			List<ObservableFeature> selectedObservableFeatures = (List<ObservableFeature>) session
+					.getAttribute("selectedObservableFeatures");
+
+			createViews(db, selectedDataSetId, selectedObservableFeatures);
+
+			session.removeAttribute("selectedObservableFeatures");
+		}
+
+	}
+
+	private void createViews(Database db, Integer selectedDataSetId, List<ObservableFeature> selectedObservableFeatures)
+	{
+		try
+		{
+			List<DataSet> dataSets = db.find(DataSet.class);
+
+			if ((dataSets != null) && !dataSets.isEmpty())
+			{
+				DataSet dataSet = null;
+				if (selectedDataSetId != null)
+				{
+					dataSet = db.findById(DataSet.class, selectedDataSetId);
+				}
+
+				if (dataSet == null)
+				{
+					dataSet = dataSets.get(0);
+					selectedDataSetId = dataSet.getId();
+				}
+
+				DataSetTable table = new DataSetTable(dataSet, db);
+
+				if (selectedObservableFeatures != null)
+				{
+					for (final Field field : table.getAllColumns())
+					{
+						ObservableFeature observableFeature = Iterables.find(selectedObservableFeatures,
+								new Predicate<ObservableFeature>()
+								{
+									@Override
+									public boolean apply(ObservableFeature of)
+									{
+										return of.getIdentifier().equals(field.getName());
+									}
+
+								}, null);
+
+						if (observableFeature == null)
+						{
+							table.hideColumn(field.getName());
+						}
+						else
+						{
+							table.showColumn(field.getName());
+						}
+					}
+				}
+
+				tableView = new JQGridView("dataset", this, table);
+				dataSetChooser = new DataSetChooser(dataSets, selectedDataSetId);
+			}
+		}
+		catch (TableException e)
+		{
+			logger.error("TableException creating views");
+			throw new RuntimeException(e);
+		}
+		catch (DatabaseException e)
+		{
+			logger.error("TableException creating views");
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * This is for the dataset chooser
+	 */
+	@Override
+	public String getCustomHtmlHeaders()
+	{
+		StringBuilder s = new StringBuilder();
+		s.append("<link rel=\"stylesheet\" href=\"bootstrap/css/bootstrap.min.css\" type=\"text/css\" />");
+		s.append("<script type=\"text/javascript\" src=\"bootstrap/js/bootstrap.min.js\"></script>");
+
+		return s.toString();
 	}
 
 	// what is shown to the user
 	public ScreenView getView()
 	{
 		MolgenisForm view = new MolgenisForm(this);
-
+		view.add(dataSetChooser);
 		view.add(tableView);
 
 		return view;
 	}
 
-	@Override
-	public void beforeLoadConfig(MolgenisRequest request, TupleTable tupleTable)
-	{
-		HttpSession session = request.getRequest().getSession();
-
-		// Check if there are selected observable features (from the catalogue
-		// viewer)
-		@SuppressWarnings("unchecked")
-		List<ObservableFeature> selectedObservableFeatures = (List<ObservableFeature>) session
-				.getAttribute("selectedObservableFeatures");
-
-		if (selectedObservableFeatures != null)
-		{
-			try
-			{
-				for (final Field field : tupleTable.getAllColumns())
-				{
-					ObservableFeature observableFeature = Iterables.find(selectedObservableFeatures,
-							new Predicate<ObservableFeature>()
-							{
-								@Override
-								public boolean apply(ObservableFeature of)
-								{
-									return of.getIdentifier().equals(field.getName());
-								}
-
-							}, null);
-
-					if (observableFeature == null)
-					{
-						tupleTable.hideColumn(field.getName());
-					}
-					else
-					{
-						tupleTable.showColumn(field.getName());
-					}
-				}
-
-				session.removeAttribute("selectedObservableFeatures");
-			}
-			catch (TableException e)
-			{
-				logger.error("TableException setting selected ObservableFeatures", e);
-			}
-
-		}
-	}
 }
