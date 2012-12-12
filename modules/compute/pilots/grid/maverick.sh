27d7d2a3d57afbbf4f3af6f03e9ac785d58155ac
georgebyelas <h.v.byelas@gmail.com>
2012-12-12 12:14:09 +0100

    small maverick edit

--- modules/compute/pilots/grid/maverick.sh
+++ modules/compute/pilots/grid/maverick.sh
@@ -1,5 +1,5 @@
 export WORKDIR=$TMPDIR
 source dataTransferSRM.sh
-curl  -F status=started http://129.125.141.171:8080/compute/api/pilot > script.sh
+curl  -F status=started http://<ip>:<port>/compute/api/pilot > script.sh
 sh script.sh 2>&1 | tee -a log.log
-curl -F status=done -F log_file=@log.log http://129.125.141.171:8080/compute/api/pilot
+curl -F status=done -F log_file=@log.log http://<ip>:<port>/compute/api/pilot
