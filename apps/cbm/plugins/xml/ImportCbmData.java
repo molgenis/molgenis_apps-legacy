0d35e7d58a375fe8754404ccfc6a80a3969f7329
Chao Pang <ChaoPang229@gmail.com>
2013-01-22 17:27:23 +0100

    removed the wrong default value

--- apps/cbm/plugins/xml/ImportCbmData.java
+++ apps/cbm/plugins/xml/ImportCbmData.java
@@ -4,8 +4,20 @@ import gme.cacore_cacore._3_2.gov_nih_nci_cbm_domain.CbmNode;
 import gme.cacore_cacore._3_2.gov_nih_nci_cbm_domain.CollectionProtocol;
 import gme.cacore_cacore._3_2.gov_nih_nci_cbm_domain.ParticipantCollectionSummary;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.molgenis.cbm.CbmXmlParser;
 import org.molgenis.cbm.Participant_Collection_Summary;
+import org.molgenis.cbm.Race;
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.server.MolgenisRequest;
+import org.molgenis.framework.ui.PluginModel;
+import org.molgenis.framework.ui.ScreenController;
+import org.molgenis.util.Entity;
 
 public class ImportCbmData extends PluginModel<Entity>
 {
@@ -57,42 +69,69 @@ public class ImportCbmData extends PluginModel<Entity>
 
 			CbmXmlParser cbmXmlParser = new CbmXmlParser();
 
-			File currentXsdfile = new File("/Users/despoina/Documents/__CTMM_project/CBM/CBM.xsd");
+			File currentXsdfile = new File("/Users/chaopang/Documents/Chao_Work/Data/CBM_data/CBM/CBM.xsd");
 
 			CbmNode result = cbmXmlParser.load(currentFile, currentXsdfile);
 
 			List<CollectionProtocol> collectionProtocol = result.getProtocols().getCollectionProtocol();
 
+			Map<Integer, Race> racesToAdd = new HashMap<Integer, Race>();
+
 			for (int i = 0; i < collectionProtocol.size(); i++)
 			{
 				List<ParticipantCollectionSummary> participantCollectionSummaryList = collectionProtocol.get(i)
 						.getEnrolls().getParticipantCollectionSummary();
-				System.out.println(participantCollectionSummaryList.get(i).getId());
 
-				Participant_Collection_Summary participantCollectionSummary = new Participant_Collection_Summary();
+				List<Participant_Collection_Summary> listOfParticipantSummary = new ArrayList<Participant_Collection_Summary>();
 
-				participantCollectionSummary.setParticipant_Count(participantCollectionSummaryList.get(i)
-						.getParticipantCount());
-				participantCollectionSummary.setRegistered_To(participantCollectionSummaryList.get(i)
-						.getRegistered_To());
-
-				if (participantCollectionSummaryList.get(i).getId() == null)
+				for (ParticipantCollectionSummary participantSummary : participantCollectionSummaryList)
 				{
-					System.out.println("participantCollectionSummaryID is null");
-				}
-				else
-				{
-					participantCollectionSummary.setParticipant_Collection_Summary_ID(participantCollectionSummaryList
-							.get(i).getId());// --exception ! this field is AUTO
+					Participant_Collection_Summary participantCollectionSummary = new Participant_Collection_Summary();
 
-				}
-				participantCollectionSummary.setEthnicity(participantCollectionSummaryList.get(i).getEthnicity());
-				participantCollectionSummary.setEthnicityId(participantCollectionSummaryList.get(i).getEthnicityId());
-				participantCollectionSummary.setGender(participantCollectionSummaryList.get(i).getGender());
-				participantCollectionSummary.setGender_Id(participantCollectionSummaryList.get(i).getGenderId());
+					participantCollectionSummary.setParticipant_Count(participantSummary.getParticipantCount());
+					// participantCollectionSummary.setRegistered_To(participantCollectionSummaryList.get(i)
+					// .getRegistered_To());
 
-				System.out.println("Just before import in db : " + participantCollectionSummary);
-				db.add(participantCollectionSummary);
+					participantCollectionSummary.setParticipant_Collection_Summary_ID(participantSummary.getId());
+
+					// Ethnicity should go to Race table.
+					participantCollectionSummary.setEthnicity(participantSummary.getEthnicity());
+
+					// This race is from CBM model, not molgenis
+					List<gme.cacore_cacore._3_2.gov_nih_nci_cbm_domain.Race> listOfRaces = participantSummary
+							.getIsClassifiedBy().getRace();
+
+					for (gme.cacore_cacore._3_2.gov_nih_nci_cbm_domain.Race eachRace : listOfRaces)
+					{
+						Race race = new Race();
+
+						String identifier = eachRace.getId() + eachRace.getRace();
+
+						race.setNCI_Code(eachRace.getId().toString());
+						race.setRace(eachRace.getRace());
+
+						if (!racesToAdd.containsKey(identifier.toLowerCase().trim()))
+						{
+							racesToAdd.put(race.getRace_ID(), race);
+						}
+					}
+
+					// participantCollectionSummary.setEthnicityId(participantCollectionSummaryList.get(i).getEthnicityId());
+					participantCollectionSummary.setGender(participantSummary.getGender());
+
+					listOfParticipantSummary.add(participantCollectionSummary);
+					// participantCollectionSummary.setGender_Id(participantCollectionSummaryList.get(i).getGenderId());
+					System.out.println("Just before import in db : " + participantSummary);
+				}
+				// db.add(new ArrayList<Race>(racesToAdd.values()));
+
+				db.add(listOfParticipantSummary);
+			}
+
+			for (Race race : racesToAdd.values())
+			{
+				System.out.println(race);
+				db.add(race);
 			}
 		}
 
