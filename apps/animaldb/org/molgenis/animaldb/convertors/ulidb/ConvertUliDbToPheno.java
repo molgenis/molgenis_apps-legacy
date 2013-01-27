333f65f67f19e4290140860ccd79b7a686509c88
paraiko <paraiko@gmail.com>
2013-01-27 18:40:00 +0100

    refactoring after commonservice changes

--- apps/animaldb/org/molgenis/animaldb/convertors/ulidb/ConvertUliDbToPheno.java
+++ apps/animaldb/org/molgenis/animaldb/convertors/ulidb/ConvertUliDbToPheno.java
@@ -645,7 +645,7 @@ public class ConvertUliDbToPheno
 				parentgroupNrMap.put(lineName, parentgroupNr);
 				String parentgroupNrPart = ct.prependZeros("" + parentgroupNr, 6);
 				String parentgroupName = "PG_" + lineName + "_" + parentgroupNrPart;
-				panelsToAddList.add(ct.createPanel(invName, parentgroupName, userName));
+				panelsToAddList.add(ct.preparePanel(invName, parentgroupName));
 				valuesToAddList.add(ct.createObservedValue(invName, appMap.get("SetTypeOfGroup"), now, null,
 						"TypeOfGroup", parentgroupName, "Parentgroup", null));
 				valuesToAddList.add(ct.createObservedValue(invName, appMap.get("SetActive"), now, null, "Active",
@@ -682,7 +682,7 @@ public class ConvertUliDbToPheno
 				litterNrMap.put(lineName, litterNr);
 				String litterNrPart = ct.prependZeros("" + litterNr, 6);
 				String litterName = "LT_" + lineName + "_" + litterNrPart;
-				panelsToAddList.add(ct.createPanel(invName, litterName, userName));
+				panelsToAddList.add(ct.preparePanel(invName, litterName));
 				valuesToAddList.add(ct.createObservedValue(invName, appMap.get("SetTypeOfGroup"), now, null,
 						"TypeOfGroup", litterName, "Litter", null));
 				valuesToAddList.add(ct.createObservedValue(invName, appMap.get("SetWeanDate"), now, null, "WeanDate",
