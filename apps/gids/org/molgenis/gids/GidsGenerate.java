e42219cdc10fc6829daadf213a296029146389a8
RoanKanninga <roankanninga@gmail.com>
2013-02-05 13:51:14 +0100

    made gids work on omx

--- apps/gids/org/molgenis/gids/GidsGenerate.java
+++ apps/gids/org/molgenis/gids/GidsGenerate.java
@@ -1,14 +1,14 @@
 package org.molgenis.gids;
 
-import org.apache.log4j.Logger;
 import org.molgenis.Molgenis;
 
 public class GidsGenerate
 {
 	public static void main(String[] args) throws Exception
 	{
-		Logger logger = Logger.getLogger("GidsGenerate");
 		new Molgenis("apps/gids/org/molgenis/gids/gids.properties").generate();
-		logger.info("GIDS generated successfully!");
+
+		// /molgenis_apps/apps/gwascentral/org/molgenis/hgvbaseg2p/hgvbase.properties
+
 	}
 }
