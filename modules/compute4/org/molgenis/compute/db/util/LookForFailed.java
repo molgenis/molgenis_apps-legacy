420826162a86345fcc9f43c0014ac90dff65c58c
georgebyelas <h.v.byelas@gmail.com>
2013-03-25 10:11:41 +0100

    Class to change statuses of failed jobs

---
+++ modules/compute4/org/molgenis/compute/db/util/LookForFailed.java
@@ -0,0 +1,49 @@
+package org.molgenis.compute.db.util;
+
+import app.DatabaseFactory;
+import org.molgenis.compute.runtime.ComputeTask;
+import org.molgenis.framework.db.Database;
+import org.molgenis.framework.db.DatabaseException;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA. User: georgebyelas Date: 23/08/2012 Time: 12:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class LookForFailed
+{
+	public static void main(String[] args)
+	{
+		Database db = null;
+		List<ComputeTask> tasks = null;
+
+		try
+		{
+			db = DatabaseFactory.create();
+			db.beginTx();
+
+			tasks = db.query(ComputeTask.class).find();
+
+            System.out.println("Change statuses of failed jobs");
+
+			for (ComputeTask task : tasks)
+			{
+                if(task.getStatusCode().equalsIgnoreCase("failed"))
+                {
+				    task.setStatusCode("generated");
+                    System.out.println(task.getName() + " >>> changed from failed to generated");
+                }
+			}
+
+			db.commitTx();
+            System.out.println("... statuses submitted");
+
+		}
+		catch (DatabaseException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+}
