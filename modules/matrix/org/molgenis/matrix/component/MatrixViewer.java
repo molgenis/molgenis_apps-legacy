4dba5de7a7368f03b80d619556519e05fcaacf4f
paraiko <paraiko@gmail.com>
2013-04-22 14:25:14 +0200

    small layout fix for name filter label.

--- modules/matrix/org/molgenis/matrix/component/MatrixViewer.java
+++ modules/matrix/org/molgenis/matrix/component/MatrixViewer.java
@@ -846,7 +846,7 @@ public class MatrixViewer extends HtmlWidget
 			divContents += "</br><input type=\"radio\" id=\"filterSaveSelect\" name=\"filterSaveSelect\" value=\"doColumnSave\"  / > Save only Columns.";
 			divContents += "</br><input type=\"radio\" id=\"filterSaveSelect\" name=\"filterSaveSelect\" value=\"doFilterAndColumnSave\"  / > Save Filters & Columns.";
 			divContents += "</br><label for=\"" + FILTERSAVENAME + "\">name: </label>"
-					+ new StringInput(FILTERSAVENAME, "Filter name").setLabel("name: ").render();
+					+ new StringInput(FILTERSAVENAME, "Filter name").render();
 			divContents += "</br>" + new ActionInput(FILTERSAVE, "", "Save").render();
 			divContents += "</br>";
 			divContents += "</br><strong>load or delete saved filters: </strong>";
